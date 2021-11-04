9399fe6d4844fb743ca615543f4bbed2b08ccf7b
aaxxiiss <mail@jukkaisokoski.fi>
2021-11-04 13:39:35 +0200

    created 0.1 version with basic features

--- extended-orders-report-wc.php
+++ extended-orders-report-wc.php
@@ -2,6 +2,13 @@
 /**
  * Plugin Name: extended-orders-report-WC
  *
+ * Plugin URI:        https://github.com/aaxxiiss/extended-orders-report-WC
+ * Description:       Extend WooCommerce analytics orders report by adding features: filter by shipping zone; display cutomer country on table
+ * Version:           0.1.0
+ * Author:            Jukka Isokoski
+ * Author URI:        https://dev.jukkaisokoski.fi/
+ * License:           GPL v2 or later
+ * 
  * @package WooCommerce\Admin
  */
 
@@ -9,9 +16,9 @@
  * Register the JS.
  */
 function add_extension_register_script() {
-	if ( ! class_exists( 'Automattic\WooCommerce\Admin\Loader' ) || ! \Automattic\WooCommerce\Admin\Loader::is_admin_or_embed_page() ) {
+/* 	if ( ! class_exists( 'Automattic\WooCommerce\Admin\Loader' ) || ! \Automattic\WooCommerce\Admin\Loader::is_admin_or_embed_page() ) {
 		return;
-	}
+	} */
 	
 	$script_path       = '/build/index.js';
 	$script_asset_path = dirname( __FILE__ ) . '/build/index.asset.php';
@@ -28,6 +35,7 @@ function add_extension_register_script() {
 		true
 	);
 
+
 	wp_register_style(
 		'extended-orders-report-wc',
 		plugins_url( '/build/index.css', __FILE__ ),
@@ -38,6 +46,175 @@ function add_extension_register_script() {
 
 	wp_enqueue_script( 'extended-orders-report-wc' );
 	wp_enqueue_style( 'extended-orders-report-wc' );
+
 }
 
 add_action( 'admin_enqueue_scripts', 'add_extension_register_script' );
+ 
+
+// Add shipping zone to WC admin asset data registry
+
+function add_shipping_zone_settings() {
+
+    $shipping_zones = array(
+		array(
+            'label' => __( 'All zones', 'dev-blog-example' ),
+            'value' => '-1',
+        ),
+    );
+
+	$defined_zones = WC_Shipping_Zones::get_zones();
+
+	foreach($defined_zones as $zone) {
+		$zone_info = array(
+            'label' => __( $zone['zone_name'], 'dev-blog-example' ),
+            'value' =>  strval($zone['id']),
+        );
+		array_push( $shipping_zones, $zone_info);
+	}
+
+	array_push( $shipping_zones, array(
+			'label' => __( 'Rest of the world', 'dev-blog-example' ),
+			'value' => '0',
+		) );
+	
+ 
+    $data_registry = Automattic\WooCommerce\Blocks\Package::container()->get(
+        Automattic\WooCommerce\Blocks\Assets\AssetDataRegistry::class
+    );
+ 
+    $data_registry->add( 'shippingZones', $shipping_zones );
+
+	
+
+}
+
+ 
+add_action( 'init', 'add_shipping_zone_settings' );
+
+
+// Add shipping_zone parameter as a query argument to the Orders Data Store and Orders Stats Data Store.
+// Those data stores use query arguments for caching purposes.
+// By adding the parameter, new database query will be performed when the parameter changes.
+
+function apply_shipping_zone_arg( $args ) {
+
+    $zone_id = '-1';
+ 
+    if ( isset( $_GET['zone_id'] ) ) {
+        $zone_id = sanitize_text_field( wp_unslash( $_GET['zone_id'] ) );
+    }
+ 
+    $args['zone_id'] = $zone_id;
+
+    return $args;
+}
+ 
+add_filter( 'woocommerce_analytics_orders_query_args', 'apply_shipping_zone_arg' );
+add_filter( 'woocommerce_analytics_orders_stats_query_args', 'apply_shipping_zone_arg' );
+
+//
+// SQL statements to the queries for gathering data
+//
+
+function add_join_subquery( $clauses ) {
+    global $wpdb;
+ 
+    $clauses[] = "JOIN {$wpdb->postmeta} shipping_zone_postmeta ON {$wpdb->prefix}wc_order_stats.order_id = shipping_zone_postmeta.post_id";
+
+    return $clauses;
+}
+ 
+add_filter( 'woocommerce_analytics_clauses_join_orders_subquery', 'add_join_subquery' );
+add_filter( 'woocommerce_analytics_clauses_join_orders_stats_total', 'add_join_subquery' );
+add_filter( 'woocommerce_analytics_clauses_join_orders_stats_interval', 'add_join_subquery' );
+
+// WHERE
+
+function add_where_subquery( $clauses ) {
+    $zone_id = '-1';
+ 
+    if ( isset( $_GET['zone_id'] ) ) {
+         $zone_id  = sanitize_text_field( wp_unslash( $_GET['zone_id'] ) );
+    } else return $clauses;
+ 
+	if ($zone_id === '-1') {
+    	$clauses[] = "AND shipping_zone_postmeta.meta_key = '_shipping_country'";
+		return $clauses;
+	}
+
+	else if ($zone_id === '0') {
+
+		$zone_ids = array();
+		$defined_zones = WC_Shipping_Zones::get_zones();
+		foreach($defined_zones as $zone) {
+			$zone_ids[] = strval($zone['id']);
+		}
+
+		$country_list = get_country_list($zone_ids);
+		
+		$country_query = get_country_query($country_list);
+
+		$clauses[] = "AND shipping_zone_postmeta.meta_key = '_shipping_country' AND shipping_zone_postmeta.meta_value NOT IN {$country_query}";
+		return $clauses;
+	}
+
+	else if ($zone_id !== '0') {
+		$country_list = get_country_list([$zone_id]);
+		
+		$country_query = get_country_query($country_list);
+
+		$clauses[] = "AND shipping_zone_postmeta.meta_key = '_shipping_country' AND shipping_zone_postmeta.meta_value IN {$country_query}";
+		return $clauses;
+	}
+
+}
+
+add_filter( 'woocommerce_analytics_clauses_where_orders_subquery', 'add_where_subquery' );
+add_filter( 'woocommerce_analytics_clauses_where_orders_stats_total', 'add_where_subquery' );
+add_filter( 'woocommerce_analytics_clauses_where_orders_stats_interval', 'add_where_subquery' );
+
+
+// Receives an array of shipping zone ids 
+// and returns an array of country codes that are included in the zones
+
+function get_country_list($zone_ids) {
+
+	$countries = array();
+
+	foreach($zone_ids as $zone_id) {
+
+		$delivery_zones = WC_Shipping_Zones::get_zones();
+		$zone = $delivery_zones[$zone_id];
+		
+
+		foreach( $zone["zone_locations"] as $location ) {
+			$countries[] = $location->code;
+		}
+
+	}
+
+	return $countries;
+
+}
+
+// Receives an array of country codes, and 
+// returns a formatted string with country code informarion which, e.g. "('FI', EN, 'SE')"
+function get_country_query( $country_list ) {
+
+	$output = '(';
+
+	for ( $i = 0; $i < count( $country_list ); $i++ ) {
+
+		$output .= "'" . $country_list[$i] . "'";
+
+		if ( $i < count( $country_list ) -1 ) {
+			$output .= ', ';
+		}
+	}
+
+	$output .= ')';
+
+	return $output;
+}
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,62 @@
 // Import SCSS entry file so that webpack picks up changes
 import './index.scss';
 
-console.log( 'hello world' );
+
+import { addFilter } from '@wordpress/hooks';
+import { __ } from '@wordpress/i18n';
+
+const addshippingZoneFilters = (filters) => {
+    return [
+        {
+            label: __('Shipping zone', 'dev-blog-example'),
+            staticParams: [],
+            param: 'zone_id',
+            showFilters: () => true,
+            defaultValue: '-1',
+            filters: [...(wcSettings.shippingZones || [])],
+        },
+        ...filters,
+    ];
+};
+
+addFilter(
+    'woocommerce_admin_orders_report_filters',
+    'dev-blog-example',
+    addshippingZoneFilters
+);
+
+
+const addTableColumn = reportTableData => {
+    if ('orders' !== reportTableData.endpoint) {
+        return reportTableData;
+    }
+
+    const newHeaders = [
+        ...reportTableData.headers,
+        {
+            label: "Country",
+            key: 'coutry',
+        },
+
+    ];
+    const newRows = reportTableData.rows.map((row, index) => {
+        const item = reportTableData.items.data[index];
+        const newRow = [
+            ...row,
+            {
+                display: item.extended_info.customer.country,
+                value: item.extended_info.customer.country,
+            },
+
+        ];
+        return newRow;
+    });
+
+    reportTableData.headers = newHeaders;
+    reportTableData.rows = newRows;
+
+    return reportTableData;
+};
+
+addFilter('woocommerce_admin_report_table', 'dev-blog-example', addTableColumn);
+
